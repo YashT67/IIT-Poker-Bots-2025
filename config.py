bb112ff7a38ce447961ded73a74da0e868e70e92
Yash T <dynamicyash35@gmail.com>
2026-03-07 20:06:19 +0530

    Final Commit

---
+++ config.py
@@ -0,0 +1,11 @@
+PYTHON_CMD = "python"
+# For linux and mac, the code python cmd is sometimes 'python3' instead of 'python'
+
+BOT_1_NAME = 'BotA'
+BOT_1_FILE = './new.py'
+
+BOT_2_NAME = 'BotB'
+BOT_2_FILE= './new.py'
+
+# GAME PROGRESS IS RECORDED HERE
+GAME_LOG_FOLDER = './logs'

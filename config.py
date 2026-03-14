bbcf4921964dbe2941110846426a1b9bf39aaed6
Yash Tulshyan <dynamicyash35@gmail.com>
2026-03-14 21:46:38 +0530

    Update bot file paths for BotA and BotB

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ PYTHON_CMD = "python"
 # For linux and mac, the code python cmd is sometimes 'python3' instead of 'python'
 
 BOT_1_NAME = 'BotA'
-BOT_1_FILE = './new.py'
+BOT_1_FILE = './bot.py'
 
 BOT_2_NAME = 'BotB'
-BOT_2_FILE= './new.py'
+BOT_2_FILE= './example_bot.py'
 
 # GAME PROGRESS IS RECORDED HERE
 GAME_LOG_FOLDER = './logs'
